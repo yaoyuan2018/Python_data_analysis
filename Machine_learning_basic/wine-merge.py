1668d676384c1c5bbf0780a7eec434c1abc94bf7
yy <yy494818027@163.com>
2019-05-05 18:05:02 +0800

    修改了wine-red和wine-white数据集格式

--- Machine_learning_basic/wine-merge.py
+++ Machine_learning_basic/wine-merge.py
@@ -1,20 +1,34 @@
 import csv
 import pandas as pd
+import numpy as np
 
-input_file = 'winequality-red.csv'
-output_file = 'test.csv'
+input_file = 'winequality-white.csv'
+output_file = 'wine_white_test.csv'
+data = []
 
-data_frame = pd.read_csv(input_file,index_col=None)
+data_frame = pd.read_csv(input_file,header=None)
+for index,row in data_frame.iterrows():
+    data.append(row[0].split(';'))
+save_model = pd.DataFrame(data)
+
+print(data[0])
+save_model.to_csv(output_file,index_label=0,header=None,index=None)
+
+
+
+# data = np.array(data_frame.loc[:,:])
+# print(data)
 
 # column1 = data_frame.iloc[:,[0]]
+# print(column1)
 
 # for row in column1:
-#     print(row[0])问卷选择题样式
+#     print(row[0])
 
 # for indexs in data_frame.index:
 #     print(data_frame.iloc[indexs].values[0:-1])
 # data_frame.set_index(data)
 # print(data_frame)
 
-for index, row in data_frame.iterrows():
-    print(row[0].split(';'))
+# for index, row in data_frame.iterrows():
+#     print(row[0].split(';'))
